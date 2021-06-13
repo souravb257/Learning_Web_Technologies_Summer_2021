7e52190072b59b4b7ae15e07625f9dc3f5e6e5ea
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-06-13 10:59:47 +0600

    Add files via upload

---
+++ LAB_TASK_03_PHP_INTRO/task4.php
@@ -0,0 +1,16 @@
+<?php
+      $num1=30;
+      $num2=25;
+      $num3=56;
+      if($num1>$num2 && $num1>$num3){
+        echo $num1;
+      }
+      else{
+        if($num2>$num1 && $num2>$num3){
+          echo $num2;
+        }
+        else
+          echo $num3;
+      }
+
+?>
