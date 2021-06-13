5f1b0c30addad276986f7966327edb4e98872032
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-06-13 10:49:49 +0600

    Add files via upload

---
+++ LAB_TASK_03_PHP_INTRO/task1.php
@@ -0,0 +1,8 @@
+ 
+<?php  
+ $length = 10;  
+ $width = 8;  
+ echo "area of rectangle is $length * $width= " . ($length * $width) . "<br />"; 
+ echo "perimeter of rectangle is $length * $width= " . (2*($length+$width)) . "<br />"; 
+	
+  ?>  
