5dc7b59058cb03eaef885851290fd8d0175d9232
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-06-20 23:35:18 +0600

    Add files via upload

---
+++ LAB_TASK5_SESSION/View/lgdashboard.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Logged in Dashboard Page</title>
+</head>
+<body>
+		<fieldset>
+			<table align="right">
+		
+		<tr>
+			<a href="home.php">Loggedin as Bob</a>
+
+			<a href="login.php">Logout</a>
+
+			
+		</tr>
+	</table>
+
+		<form method="post" action="../controller/loginCheck.php">
+			
+			<table border="0" width="100%" align="left">
+
+			 
+			  <tr>
+			   <th><h4>Account<hr></h4></th>
+			   <th> <h4>Welcome Bob</h4></th>
+			  
+			  </tr>
+			  
+			</table>
+			<a href="">Dashboard</a><br>
+			<a href="">View Profile</a><br>
+			<a href="">Edit Profile</a><br>
+			<a href="">Change Profile Picture</a><br>
+			<a href="">Change Password</a><br>
+			<a href="">Logout</a><br>
+
+
+
+		</form>
+		</fieldset>
+</body>
+</html>
