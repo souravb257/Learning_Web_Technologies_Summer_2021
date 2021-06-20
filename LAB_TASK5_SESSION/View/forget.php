b188fb25302cc9558b39fadce4675ef0c548bfec
sourav barman <63946634+souravb257@users.noreply.github.com>
2021-06-20 21:54:55 +0600

    Add files via upload

---
+++ LAB_TASK5_SESSION/View/forget.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forget Password Page</title>
+</head>
+<body>
+		<h3>Forget Password Page </h3>
+
+		<form method="post" action="../controller/loginCheck.php">
+			<fieldset>
+				<legend>Login</legend>
+				<table>
+					<tr>
+						<td>Enter Email</td>
+						<td><input type="email" name="email"></td>
+					</tr>
+					
+					
+					<tr>
+						<td></td>
+						<td><input type="submit" name="submit" value="Submit">
+						
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+</body>
+</html>
